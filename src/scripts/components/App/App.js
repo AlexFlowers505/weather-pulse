1121b678de6ee9a330ceddaf5820baf4a995fb8b
AlexFlowers505 <allnewalex@Yandex.com>
2024-06-21 20:47:30 +0300

    basic setting up done

---
+++ src/scripts/components/App/App.js
@@ -0,0 +1,10 @@
+import AssetLayout from '../AssetLayout/AssetLayout'
+import '../../../styles/overalls/overalls.css'
+
+function App() {
+  return (
+    <AssetLayout />
+  )
+}
+
+export default App
