6f2f84c507396ebad5864164e96b3e6212cf829a
AlexFlowers505 <allnewalex@Yandex.com>
2024-06-25 19:10:11 +0300

    checkpoint

---
+++ src/views/sections/CurrentCityDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CurrentCityDisplay() {
+  return (
+    <section className=''>
+        <div className="">
+            <h6>Москва</h6>
+        </div>
+    </section>
+  )
+}
