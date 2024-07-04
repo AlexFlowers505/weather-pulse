aea0b720e846a0fa3a2bb096f79e6d763d78f35e
AlexFlowers505 <allnewalex@Yandex.com>
2024-07-04 21:33:54 +0300

    checkpoint commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useEffect } from "react"
-import { fetchWeather } from "./api/openWeatherMap"
-
 import MainLayout from "./views/layouts/MainLayout"
 import CityOverviewPage from "./views/pages/CityOverviewPage"
 import FavouriteCitiesOverviewPage from "./views/pages/FavouriteCitiesOverviewPage"
@@ -9,20 +6,6 @@ import WelcomePage from "./views/pages/WelcomePage"
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
 
 export default function App() {
-
-  useEffect(() => {
-    async function getWeather() {
-        try {
-            const weatherData = await fetchWeather('London')
-            console.log('Weather data:', weatherData)
-        } catch (error) {
-            console.error('Error:', error)
-        }
-    }
-
-    getWeather()
-  }, [])
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />} >
--- src/views/pages/CityOverviewPage.jsx
+++ src/views/pages/CityOverviewPage.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchWeather } from "../../api/openWeatherMap"
+
 import Navbar from '../sections/Navbar'
 import ControlPanel from '../sections/ControlPanel'
 import CurrentCityForecastDisplay from '../sections/CurrentCityForecastDisplay'
@@ -6,15 +8,38 @@ import TodayForecast from '../sections/TodayForecast'
 import SevenDaysForecast from '../sections/SevenDaysForecast'
 
 export default function CityOverviewPage() {
+
+  const [forecast, setForecast] = useState({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function getWeather() {
+        try {
+            const weatherData = await fetchWeather('London')
+            console.log('Weather data:', weatherData)
+        } catch (error) {
+            console.error('Error:', error)
+        } finally {
+          setLoading(false)
+        }
+    }
+
+    getWeather()
+  }, [])
+
   return (
     <>
-        <Navbar />
-        <div className='flex flex-col justify-between items-start w-full gap-base'>
-            <ControlPanel />
-            <CurrentCityForecastDisplay />
-            <TodayForecast />
-        </div>
-        <SevenDaysForecast />
+        { loading ? (<h2>Loading...</h2>) : (
+          <>
+            <Navbar />
+            <div className='flex flex-col justify-between items-start w-full gap-base'>
+                <ControlPanel />
+                <CurrentCityForecastDisplay />
+                <TodayForecast />
+            </div>
+            <SevenDaysForecast />
+          </>
+        )}
     </>
   )
 }
