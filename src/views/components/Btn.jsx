927e5afac5e12c98a439c6094553a523dbe3d0ee
AlexFlowers505 <allnewalex@Yandex.com>
2024-07-24 22:11:57 +0300

    added material ui and implemented tooltip into the btn

--- src/views/components/Btn.jsx
+++ src/views/components/Btn.jsx
@@ -1,15 +1,53 @@
-import SpriteSvg from './SpriteSvg'
-import btnStyles from '../../styles/components/btnStyles'
-import btnContentTypes from '../../constants/btnContentTypes'
+import { forwardRef } from 'react';
+import SpriteSvg from './SpriteSvg';
+import btnStyles from '../../styles/components/btnStyles';
+import btnContentTypes from '../../constants/btnContentTypes';
+import { Tooltip } from '@mui/material';
 
-export default function Btn({extraSVGstyle, content, contentType, btnSize=btnStyles.size.md, btnStyle=btnStyles.style.filled, extraBtnStyles='', onClick}) {
-    return (  
-        <button 
-            className={`${btnStyles.base} ${btnSize.btn} ${btnStyle} ${extraBtnStyles}`}
-            onClick={onClick}
-        >
-            { contentType === btnContentTypes.icon && <SpriteSvg id={content} className={btnSize.icon} extraSVGstyle={extraSVGstyle}/> }
-            { contentType === btnContentTypes.text && content }
-        </button>
-    )
-}
+const Btn = forwardRef(({
+  extraSVGstyle,
+  content,
+  contentType,
+  btnSize = btnStyles.size.md,
+  btnStyle = btnStyles.style.filled,
+  extraBtnStyles = '',
+  hasTooltip = false,
+  tooltipContent = '',
+  onClick,
+  ...props
+}, ref) => {
+  const button = (
+    <button
+      className={`${btnStyles.base} ${btnSize.btn} ${btnStyle} ${extraBtnStyles}`}
+      onClick={onClick}
+      ref={ref}
+      {...props}
+    >
+      {contentType === btnContentTypes.icon && <SpriteSvg id={content} className={btnSize.icon} extraSVGstyle={extraSVGstyle} />}
+      {contentType === btnContentTypes.text && content}
+    </button>
+  );
+
+  return hasTooltip ? (
+    <Tooltip
+      title={tooltipContent}
+      placement="top"
+      PopperProps={{
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [10, 0], // adjust the offset here, [x-axis, y-axis]
+            },
+          },
+        ],
+      }}
+    >
+      <span>{button}</span>
+    </Tooltip>
+  ) : (
+    button
+  );
+});
+
+export default Btn;
--- src/views/components/Search.jsx
+++ src/views/components/Search.jsx
@@ -1,98 +1,104 @@
-import { useEffect, useState, useRef } from "react"
-import btnStyles from '../../styles/components/btnStyles'
-import btnContentTypes from "../../constants/btnContentTypes"
-import DismissBtn from './btns/CloseBtn'
-import SearchResults from './SearchResults'
-import { fetchAreasSuggestions } from "../../api/dadata"
-import { fetchLocationsForecasts } from "../../api/openWeatherMap"
-import { removeMultipleSpaces } from "../../utils/utils"
-import options from "../../constants/fetchingSuggestionsOptions"
-import searchResultsStates from "../../constants/searchResultsStates"
-import { handleClearSearchBtnClick } from "../../utils/utils"
-import { searchStyle as tw } from "../../styles/components/Search.style"
-import temperatureUnits from "../../constants/temperatureUnits"
-import Btn from "./Btn"
+import { useEffect, useState, useRef } from "react";
+import btnStyles from '../../styles/components/btnStyles';
+import btnContentTypes from "../../constants/btnContentTypes";
+import DismissBtn from './btns/CloseBtn';
+import SearchResults from './SearchResults';
+import { fetchAreasSuggestions } from "../../api/dadata";
+import { fetchLocationsForecasts } from "../../api/openWeatherMap";
+import { removeMultipleSpaces } from "../../utils/utils";
+import options from "../../constants/fetchingSuggestionsOptions";
+import searchResultsStates from "../../constants/searchResultsStates";
+import { handleClearSearchBtnClick } from "../../utils/utils";
+import { searchStyle as tw } from "../../styles/components/Search.style";
+import temperatureUnits from "../../constants/temperatureUnits";
+import Btn from "./Btn";
 
-const {IDLE, LOADING, ERROR, SUCCESS, NO_RESULTS } = searchResultsStates
+const {IDLE, LOADING, ERROR, SUCCESS, NO_RESULTS } = searchResultsStates;
 
 const searchBarAttrs = {
   placeHolder: 'Начните вводить название населенного пункта'
-}
+};
 
 export default function Search({styles=''}) {
 
-  const [request, setRequest] = useState('')
-  const [fetchState, setFetchState] = useState(IDLE)
-  const [repeatFetch, setRepeatFetch] = useState(false)
-  const [suggestions, setSuggestions] = useState([])
-  const inputRef = useRef(null)
+  const [request, setRequest] = useState('');
+  const [fetchState, setFetchState] = useState(IDLE);
+  const [repeatFetch, setRepeatFetch] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
+  const inputRef = useRef(null);
 
   const handleRequestChange = evt => {
-    setRequest(evt.target.value)
-  }
+    setRequest(evt.target.value);
+  };
 
   // page loaded handler
-  useEffect( () => {
-    inputRef.current.focus()
-  }, [])
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   // request handler
-  useEffect( () => {
-    const formattedRequest = removeMultipleSpaces(request)
+  useEffect(() => {
+    const formattedRequest = removeMultipleSpaces(request);
 
-    const debounceFetch = setTimeout( () => {
+    const debounceFetch = setTimeout(() => {
       if (formattedRequest.length > options.minRequestSymbolsQnt) {
-        setFetchState(LOADING)
+        setFetchState(LOADING);
 
         fetchAreasSuggestions(formattedRequest)
           .then(data => fetchLocationsForecasts(data))
-          .then( data => { 
-            setSuggestions(data) 
-            setFetchState(data.length ? SUCCESS : NO_RESULTS)
+          .then(data => { 
+            setSuggestions(data);
+            setFetchState(data.length ? SUCCESS : NO_RESULTS);
           })
           .catch(error => { 
-            setFetchState(ERROR)
-            console.error(error) 
-          })
+            setFetchState(ERROR);
+            console.error(error);
+          });
       } else {
-        setFetchState(IDLE)
-        setSuggestions([])
+        setFetchState(IDLE);
+        setSuggestions([]);
       }
-    }, options.debounceTimeInMilisec)
+    }, options.debounceTimeInMilisec);
 
-    return () => clearTimeout(debounceFetch)
-
-  }, [request, repeatFetch])
+    return () => clearTimeout(debounceFetch);
+  }, [request, repeatFetch]);
 
   return (
     <div className={`search-block ${tw.searchBlock} ${styles}`}>
       <div className={`input-wrapper ${tw.inputWrapper}`}>
-        <input 
-          className={`search-bar ${tw.searchBar}`} 
+        <input
+          className={`search-bar ${tw.searchBar}`}
           ref={inputRef}
-          type="text" 
+          type="text"
           value={request}
-          onChange={ handleRequestChange }
-          placeholder={searchBarAttrs.placeHolder} />
-          <div className={`btn-wrapper ${tw.btnWrapper}`}>
-          {!!request.length && <DismissBtn
-            extraBtnStyles={tw.DismissBtn}
-            btnSize={btnStyles.size.md}
-            btnStyle={btnStyles.style.contentOnly}
-            onClick={ ()=> handleClearSearchBtnClick(inputRef, setFetchState, null, setRequest) }
-          />}
-          <Btn 
+          onChange={handleRequestChange}
+          placeholder={searchBarAttrs.placeHolder}
+        />
+        <div className={`btn-wrapper ${tw.btnWrapper}`}>
+          {!!request.length && (
+            <DismissBtn
+              extraBtnStyles={tw.DismissBtn}
+              hasTooltip={true}
+              tooltipContent={'Очистить'}
+              btnSize={btnStyles.size.md}
+              btnStyle={btnStyles.style.contentOnly}
+              onClick={() => handleClearSearchBtnClick(inputRef, setFetchState, null, setRequest)}
+            />
+          )}
+          <Btn
             extraBtnStyles={tw.unitsBtn}
             contentType={btnContentTypes.text}
             content={temperatureUnits.celsius.symbol}
+            hasTooltip={true}
+            tooltipContent={'Switch Units'}
             btnSize={btnStyles.size.md}
             btnStyle={btnStyles.style.contentOnly}
-            onClick={ ()=> handleClearSearchBtnClick(inputRef, setFetchState, null, setRequest) }            
+            onClick={() => handleClearSearchBtnClick(inputRef, setFetchState, null, setRequest)}
           />
-          </div>
+        </div>
       </div>
-      <SearchResults 
-        suggestions={suggestions} 
-        fetchState={fetchState} 
+      <SearchResults
+        suggestions={suggestions}
+        fetchState={fetchState}
         request={request}
         inputRef={inputRef}
         setFetchState={setFetchState}
@@ -100,5 +106,5 @@ export default function Search({styles=''}) {
         setRepeatFetch={setRepeatFetch}
       />
     </div>
-  )
+  );
 }
