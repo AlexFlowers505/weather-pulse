79ac9495a4496d703c33f00e1f5422bae049b834
AlexFlowers505 <allnewalex@Yandex.com>
2024-07-30 22:55:54 +0300

    quickfix

--- src/views/components/GeolocationDeniedInfoBlock.jsx
+++ src/views/components/GeolocationDeniedInfoBlock.jsx
@@ -101,54 +101,58 @@ export default function GeolocationDeniedInfoBlock() {
             { data.map( (item, i) => { 
                 return (
                     <li key={i} className={`content-item ${tw.contentItem}`}>
-                        {item.isHeaderItem ? (
-                            <p className={`content-item-heading ${tw.contentItemHeading}`}>{item.heading}</p>
-                        ) : (<>
-                                <div className={`content-item-header ${tw.contentItemHeader}`}>
-                                    {item.iconType === iconTypes.svg ? <svg className={`content-item-icon ${tw.contentItemIcon}`}><use href={`#${item.icon}`}></use></svg> :
-                                    item.iconType === iconTypes.png ? <img className={`content-item-icon ${tw.contentItemIcon}`} src={item.icon} /> : null}
-                                    <p className={`content-item-heading ${tw.contentItemHeading}`}>{item.heading}</p>
-                                </div>
+                        {
+                            item.isHeaderItem ? (
+                                <p className={`content-item-heading ${tw.contentItemHeading}`}>{item.heading}</p>
+                            ) : (
+                                <>
+                                    <div className={`content-item-header ${tw.contentItemHeader}`}>
+                                        {item.iconType === iconTypes.svg ? <svg className={`content-item-icon ${tw.contentItemIcon}`}><use href={`#${item.icon}`}></use></svg> :
+                                        item.iconType === iconTypes.png ? <img className={`content-item-icon ${tw.contentItemIcon}`} src={item.icon} /> : null}
+                                        <p className={`content-item-heading ${tw.contentItemHeading}`}>{item.heading}</p>
+                                    </div>
 
-                                <div className={`content-item-details ${tw.contentItemDetails}`}>
-                                    {
-                                        item.detailsFlow === detailsFlow.block ? (
-                                            item.details.map((detail, i) => {
-                                                return (
-                                                    <div className={`content-item-detail-wrapper ${tw.contentItemDetailsWrapper}`} key={i}>
-                                                        <span className={`list-item-symbol ${tw.listItemSymbol}`}>{listItemSeparator}</span>
-                                                        <span className={`content-item-detail ${tw.contentItemDetail}`}>{detail}</span>
-                                                    </div>
-                                                )
-                                            }))
-                                        : item.detailsFlow === detailsFlow.inline ? (
-                                            <div className={`content-item-details-wrapper ${tw.contentItemDetailsWrapper}`}>
-                                                {
-                                                    item.details.map((detail, i, arr) => {
-                                                        return (
-                                                            <React.Fragment key={i}>
-                                                                <span className={`content-item-detail ${tw.contentItemDetail}`}>{detail}</span>
-                                                                {i < arr.length-1 && <span className={`list-item-symbol ${tw.listItemSymbol}`}>{listItemSeparator}</span>}
-                                                            </React.Fragment>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        ) : null 
-                                    }
-                                </div>
+                                    <div className={`content-item-details ${tw.contentItemDetails}`}>
+                                        {
+                                            item.detailsFlow === detailsFlow.block ? (
+                                                item.details.map((detail, i) => {
+                                                    return (
+                                                        <div className={`content-item-detail-wrapper ${tw.contentItemDetailsWrapper}`} key={i}>
+                                                            <span className={`list-item-symbol ${tw.listItemSymbol}`}>{listItemSeparator}</span>
+                                                            <span className={`content-item-detail ${tw.contentItemDetail}`}>{detail}</span>
+                                                        </div>
+                                                    )
+                                                }))
+                                            : item.detailsFlow === detailsFlow.inline ? (
+                                                <div className={`content-item-details-wrapper ${tw.contentItemDetailsWrapper}`}>
+                                                    {
+                                                        item.details.map((detail, i, arr) => {
+                                                            return (
+                                                                <React.Fragment key={i}>
+                                                                    <span className={`content-item-detail ${tw.contentItemDetail}`}>{detail}</span>
+                                                                    {i < arr.length-1 && <span className={`list-item-symbol ${tw.listItemSymbol}`}>{listItemSeparator}</span>}
+                                                                </React.Fragment>
+                                                            )
+                                                        })
+                                                    }
+                                                </div>
+                                            ) : null 
+                                        }
+                                    </div>
 
-                                {item.hasBtn &&(
-                                    <Btn 
-                                        contentType={btnContentTypes.text}
-                                        content={item.btnContent}
-                                        btnStyle={btnStyles.style.outlined}
-                                        btnSize={btnStyles.size.sm}
-                                        extraBtnClass={tw.refreshPageBtn}
-                                        onClick={item.handleBtnClick}
-                                    />
-                                )}
-                            </>)}
+                                    {item.hasBtn &&(
+                                        <Btn 
+                                            contentType={btnContentTypes.text}
+                                            content={item.btnContent}
+                                            btnStyle={btnStyles.style.outlined}
+                                            btnSize={btnStyles.size.sm}
+                                            extraBtnClass={tw.refreshPageBtn}
+                                            onClick={item.handleBtnClick}
+                                        />
+                                    )}
+                                </>
+                            )
+                        }
                     </li>
                 )
             })}
