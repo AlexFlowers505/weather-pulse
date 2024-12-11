5149ad91c4b765ce3978d66c41f98852440a1394
a.tsvetkov505 <as@prcs.ru>
2024-12-11 12:59:53 +0300

    favourites list is getting rendered

--- src/views/sections/AreasForecastsList.tsx
+++ src/views/sections/AreasForecastsList.tsx
@@ -1,49 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import AreaShortForecastItem from '../components/AreaShortForecastItem'
+import { useEffect, useState } from 'react'
 import { areasForecastsListStyle as tw } from '../../styles/sections/AreasForecastsList.style'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../redux/store/store'
 import LocationCurrentWeather from './LocationCurrentWeather'
 import { FavouriteLocation } from '../../types/components/favouriteLocation.type'
 import { fetchWeather } from '../../api/openWeatherMap/fetchWeather'
-import { OpenWeatherMapResponse, WeatherResponse } from '../../types/api/openWeatherMap/OpenWeatherMapResponse.type'
-import { WholeLocationData } from '../../types/overalls/wholeLocationData.type'
-import { MappedLocationShortData } from '../../types/api/openWeatherMap/MappedLocationShortData.type'
+import { WeatherResponse } from '../../types/api/openWeatherMap/OpenWeatherMapResponse.type'
 import { MappedFavouriteLocation } from '../../types/components/mappedFavouriteLocation.type'
 
 export default function AreasForecastsList(): React.JSX.Element {
-  const [ favouriteLocations, setFavouriteLocations ] = useState<MappedFavouriteLocation[]>([])
   const units = useSelector((state: RootState) => state.temperatureUnits.__type)
-  const storedFavouriteLocations: FavouriteLocation[] | [] = useSelector((state: RootState) => state.favouriteLocations.value)
-  
+  const favouriteLocations: FavouriteLocation[] | [] = useSelector((state: RootState) => state.favouriteLocations.value)
+  const [ favouriteLocationsMapped, setFavouriteLocationsMapped ] = useState<MappedFavouriteLocation[]>([])
+
   useEffect(() => {
-    storedFavouriteLocations.map(async (elm: FavouriteLocation) => {
-      const fetchedWeather = await fetchWeather({ id: elm.id, isForecast: false, units: units }) as WeatherResponse
-      const mappedFavouriteLocation: MappedFavouriteLocation = {
-        id: elm.id,
-        isSpecific: elm.isSpecific,
-        specificLocation: elm.specificLocation,
-        area: elm.area,
-        region: elm.region,
-        country: elm.country,
-        temperature: fetchedWeather.main.temp,
-        weatherIcon: fetchedWeather.weather[0].icon,
-        lat: fetchedWeather.coord.lat,
-        lon: fetchedWeather.coord.lon
-      } 
-      setFavouriteLocations([...favouriteLocations, mappedFavouriteLocation])
-    })
+    const fetchAllFavouriteLocationsForecast = async () => {
+      const fetchedLocations = await Promise.all(
+        favouriteLocations.map(async (elm: FavouriteLocation) => {
+          const fetchedWeather = await fetchWeather({ id: elm.id, isForecast: false, units: units }) as WeatherResponse
+          return {
+            id: elm.id,
+            isSpecific: elm.isSpecific,
+            specificLocation: elm.specificLocation,
+            area: elm.area,
+            region: elm.region,
+            country: elm.country,
+            temperature: fetchedWeather.main.temp,
+            weatherIcon: fetchedWeather.weather[0].icon,
+            lat: fetchedWeather.coord.lat,
+            lon: fetchedWeather.coord.lon,
+          }
+        })
+      )
+      setFavouriteLocationsMapped(fetchedLocations)
+    }
+  
+    if (favouriteLocations.length > 0) {
+      fetchAllFavouriteLocationsForecast()
+    }
   }, [])
 
   return (
     <section className={`container-visuals ${tw.base}`}>
       <div className={`favourites-inner-wrapper ${tw.innerWrapper}`}>
-        {favouriteLocations.length > 0 ?
-          favouriteLocations.map(location => {
-            return <LocationCurrentWeather {...location} />
+        {favouriteLocationsMapped.length > 0 ?
+          favouriteLocationsMapped.map( (location: MappedFavouriteLocation, i: number) => {
+            return <LocationCurrentWeather {...location} key={i} />
           })
         : <div></div> }
       </div>
     </section>
   )
 }
+
